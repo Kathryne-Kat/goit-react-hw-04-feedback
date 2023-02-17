f62e807f9c303dc3f5881fa1e4466b1d37571496
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-02-17 11:10:58 +0200

    add all

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,76 +1,30 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Section } from '../Section/Section';
 import { Statistics } from '../Statistics/Statistics';
 import { Notification } from '../Notification/Notification';
 import css from './App.module.css';
 
-// export default class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   onLeaveFeedback = options => {
-//     this.setState(prevState => ({ [options]: prevState[options] + 1 }));
-//   };
-
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     return parseInt((this.state.good / this.countTotalFeedback()) * 100) || 0;
-//   };
-
-//   render() {
-//     const total = this.countTotalFeedback();
-
-//     return (
-//       <div className={css.container}>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//           {total ? (
-//             <Statistics
-//               title="Statistics"
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={total}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
-
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [options, setOption] = useState({ good: 0, neutral: 0, bad: 0 });
-
-  if ((options = 'good')) setGood(options);
-  if ((options = 'neutral')) setNeutral(options);
-  if ((options = 'bed')) setBad(options);
+  const [options] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const onLeaveFeedback = options => {
-    //this.setState(prevState => ({ [options]: prevState[options] + 1 }));
-    setOption(prevOptions => ({
-      good: prevOptions.good + 1,
-      neutral: prevOptions.neutral + 1,
-      bad: prevOptions.bad + 1,
-    }));
-    // setGood(prevState => prevState[options] + 1);
-    // setNeutral(prevState => prevState[options] + 1);
-    // setBad(prevState => prevState[options] + 1);
+    switch (options) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
   const countTotalFeedback = () => {
